c486a568b499e4f454c06d84566e1e5e67ce40a9
sigurrosk <ssk@gmx.se>
2014-06-07 15:47:28 +0000

    Small changes to the plugin

--- thorsa/__init__.py
+++ thorsa/__init__.py
@@ -9,16 +9,21 @@ class HelloWorld(FileTypePlugin):
     description         = 'Mess with metadata on import'
     supported_platforms = ['windows', 'osx', 'linux'] # Platforms this plugin will run on
     author              = 'Thorsa' # The author of this plugin
-    version             = (1, 0, 0)   # The version number of this plugin
+    version             = (1, 1, 0)   # The version number of this plugin
     file_types          = set(['epub', 'mobi']) # The file types that this plugin will be applied to
     on_import      = True # Run this plugin after importing into the library
     minimum_calibre_version = (0, 7, 53)
+
+
 
     def run(self, path_to_ebook):
         from calibre.ebooks.metadata.meta import get_metadata, set_metadata
         file = open(path_to_ebook, 'r+b')
         ext  = os.path.splitext(path_to_ebook)[-1][1:].lower()
         mi = get_metadata(file, ext)
-        mi.rating = 0
+        mi.rating = 5
         set_metadata(file, mi, ext)
+        f = open('workfile', 'w')
+        s = str('File: ' + file + ' ext: ' + ext)
+        f.write()
         return path_to_ebook
